99fa3cf288ac93b95b94d3f13ff7d182c6ad5595
Takashi Kawasaki <espresso3389@gmail.com>
2020-02-08 12:32:56 +0900

    Remove unused dependencies.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
+## 0.1.0
+
+* Remove unused dependencies.
+
 ## 0.0.1
 
 * First release.
-
--- test/reader_writer_lock_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:reader_writer_lock/reader_writer_lock.dart';
-
-void main() {
-
-}
